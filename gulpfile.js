0e0f7e4e0e16d1ea23670ab19a4f2b3f3fdf6607
Robbie Bardijn <robbie.bardijn@crosscheck.be>
2016-03-07 14:05:04 +0100

    master: improvements

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var download = require("gulp-download");
 var gulpif = require('gulp-if');
 var replace = require('gulp-replace');
+var clean = require('gulp-clean');
 
 var rename = require("gulp-rename");
 var argv = require('yargs').argv;
@@ -16,42 +17,60 @@ var browserSync = require('browser-sync').create();
 //});
 
 var checkArg = function(arg) {
-  if(arg == undefined) {
+  if(arg === undefined) {
     return false;
   } else {
     return true;
   }
 }
-var getCss = function(url) {
-  var str = '';
 
-  str +='<link rel="stylesheet" href="' + url + 'base-styles.css">\n';
-  str +='<link rel="stylesheet" href="' + url + 'helper-styles.css">\n';
-  str +='<link rel="stylesheet" href="' + url + 'component-styles.css">\n';
+var getAllCss = function(url) {
+  var str = '<!-- INJECTED BY VACCINE -->\n';
+
+  str +='  <link rel="stylesheet" href="' + url + 'base-styles.css">';
+  str +='<link rel="stylesheet" href="' + url + 'helper-styles.css">';
+  str +='<link rel="stylesheet" href="' + url + 'component-styles.css">';
+  str +='<link rel="stylesheet" href="' + url + 'layout-styles.css">';
   str +='<link rel="stylesheet" href="' + url + 'layout-styles.css">\n';
-  str +='<link rel="stylesheet" href="' + url + 'layout-styles.css">\n';
+
+  str +='  <!-- END INJECTION -->\n';
+
+  return str;
+}
+
+var getCss = function(url) {
+  var str = '<!-- INJECTED BY VACCINE -->\n';
+
+  str +='  <link rel="stylesheet" href="' + url + '">\n';
+
+  str +='  <!-- END INJECTION -->\n';
 
   return str;
 }
 
 gulp.task("default", function() {
-  download(argv.url)
-    .pipe(gulpif(checkArg(argv.url), rename(argv.url.split("//")[1].replace("/", "-") + ".html")))
-    .pipe(gulpif(checkArg(argv.injectcss), replace("</head>", getCss(argv.injectcss) + "</head>")))
-    .pipe(gulp.dest("pages/"));
+  var server = {
+    server: {
+      baseDir: "./pages/",
+      directory: true
+    }
+  };
 
-    browserSync.init({
-      server: {
-        baseDir: "./pages/",
-        index: argv.url.split("//")[1].replace("/", "-") + ".html"
-      }
-    });
+  if(argv.url !== undefined) {
+    download(argv.url)
+      .pipe(gulpif(checkArg(argv.url), rename(argv.url.split("//")[1] + ".html")))
+      .pipe(gulpif(checkArg(argv.injectallcss), replace("</head>", getAllCss(argv.injectallcss) + "</head>")))
+      .pipe(gulpif(checkArg(argv.injectcss), replace("</head>", getCss(argv.injectcss) + "</head>")))
+      .pipe(gulp.dest("pages/"));
+
+    server.server.directory = false;
+    server.server.index = argv.url.split("//")[1] + ".html";
+  }
+
+  browserSync.init(server);
 });
 
-gulp.task("browser-sync", function() {
-  browserSync.init({
-    server: {
-      baseDir: "pages/"
-    }
-  });
+gulp.task("clean-pages", function() {
+    return gulp.src(['pages/*','!pages/.gitkeep'], {read: false})
+      .pipe(clean());
 });
